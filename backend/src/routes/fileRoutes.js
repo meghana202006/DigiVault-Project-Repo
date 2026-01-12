c3d01742850e7e3b343adfa38bd6551d88dbb1e7
Molaybera <Molaybera299@gmail.com>
2026-01-12 22:00:53 +0530

    added the api where user can access list of data they have uploded

--- backend/src/routes/fileRoutes.js
+++ backend/src/routes/fileRoutes.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const upload = require('../config/multer');
 const requireAuth = require('../middleware/requireAuth');
 const { uploadFile } = require('../controllers/files/fileController');
+const { getAllFiles } = require('../controllers/files/dashboardController');
 
-// register rout
+// upload route
 // you can acess this api by going to "http://localhost:5000/api/files/upload"
 router.post('/upload', requireAuth, upload.array('files', 5), uploadFile);
+
+// dashboard route to see user list of data
+// you can acess this api by going to "http://localhost:5000/api/files/dashboard"
+router.post('/dashboard', requireAuth, getAllFiles);
+
 
 module.exports = router;
